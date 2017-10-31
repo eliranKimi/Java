30747f16e9b1b2979cf9aee634462f8ff8bf1753
eliran <eliran.kimi@gmail.com>
2017-10-31 17:46:45 +0200

    Added ovverrid

--- maman11/src/Rational.java
+++ maman11/src/Rational.java
@@ -1,29 +1,21 @@
 
-
 public class Rational {
-	
+
 	private int deno; // Denominator
-	private int num; // Numerator 
-	
-	
+	private int num; // Numerator
+
 	public Rational(int num, int den) {
 		super();
-		if( den > 0 || num >= 0  )
-		{
+		if (den > 0 || num >= 0) {
 			this.deno = den;
 			this.num = num;
+		} else {
+			this.deno = 0;
+			this.num = 0;
+
 		}
-		else
-		{
-			this.deno=0;
-			this.num=0;
-			
-		
-		}
-		
-		
+
 	}
-	
 
 	public int getDenominator() {
 		return deno;
@@ -40,106 +32,94 @@ public class Rational {
 	public void setNumerator(int num) {
 		this.num = num;
 	}
-	
-	public Boolean greaterThan( Rational number )
-	{
-		if( this.getNumerator() * number.getDenominator() > this.getDenominator() * number.getNumerator() )
-		{
+
+	public Boolean greaterThan(Rational number) {
+		if (this.getNumerator() * number.getDenominator() > this.getDenominator() * number.getNumerator()) {
 			return true;
 		}
-		
+
 		return false;
-		
+
 	}
-	
-	// Check how to override
-	public Boolean equals(Rational number)
-	{
-		
-		if( this.getNumerator() * number.getDenominator() ==  this.getDenominator() * number.getNumerator() )
-		{
-			return true;
+
+	@Override
+	public boolean equals(Object number) {
+		if (number instanceof Rational) {
+			if (this.getNumerator() * ((Rational) number).getDenominator() == this.getDenominator()
+					* ((Rational) number).getNumerator()) {
+				return true;
+			}
+
+			return false;
+
+		} else {
+			return false;
 		}
-		
-		return false;
-		
-		
+
 	}
-	
-	public Rational plus(Rational number)
-	{
+
+	public Rational plus(Rational number) {
 		Rational sum;
 		int den;
 		int num;
-		
-		den=this.getDenominator()+number.getDenominator();
-		num=this.getNumerator() * number.getDenominator() + this.getDenominator() * number.getNumerator();
-		
-		sum=new Rational(den,num);
-		
+
+		den = this.getDenominator() + number.getDenominator();
+		num = this.getNumerator() * number.getDenominator() + this.getDenominator() * number.getNumerator();
+
+		sum = new Rational(den, num);
+
 		return sum;
 	}
 
-	public Rational minus(Rational number)
-	{
+	public Rational minus(Rational number) {
 		Rational sum;
 		int den;
 		int num;
-		
-		den=this.getDenominator()+number.getDenominator();
-		num=this.getNumerator() * number.getDenominator() - this.getDenominator() * number.getNumerator();
-		
-		sum=new Rational(den,num);
-		
+
+		den = this.getDenominator() + number.getDenominator();
+		num = this.getNumerator() * number.getDenominator() - this.getDenominator() * number.getNumerator();
+
+		sum = new Rational(den, num);
+
 		return sum;
 	}
 
-	public Rational multiply(Rational number)
-	{
+	public Rational multiply(Rational number) {
 		Rational result;
-		int num=this.getNumerator()*number.getNumerator();
-		int den=this.getDenominator()* number.getDenominator();
-		result=new Rational(den,num);
+		int num = this.getNumerator() * number.getNumerator();
+		int den = this.getDenominator() * number.getDenominator();
+		result = new Rational(den, num);
 		return result;
 	}
-	
-	public String toString()
-	{
-		return this.getNumerator()+"/"+this.getDenominator();
-		
+
+	@Override
+	public String toString() {
+		return this.getNumerator() + "/" + this.getDenominator();
+
 	}
-	
-	private int gcd(int x, int y)
-	{
-		if ( y !=0 )
-		{
-			return this.gcd(y, x%y);
-		}
-		else
-		{
+
+	private int gcd(int x, int y) {
+		if (y != 0) {
+			return this.gcd(y, x % y);
+		} else {
 			return x;
 		}
 
 	}
-	
-	public Rational reduce()
-	{ 
+
+	public Rational reduce() {
 		Rational number;
-		int divider=this.gcd(this.getDenominator(), this.getNumerator());
-		number = new Rational(this.getDenominator()/divider,this.getNumerator()/divider);
-		
+		int divider = this.gcd(this.getDenominator(), this.getNumerator());
+		number = new Rational(this.getDenominator() / divider, this.getNumerator() / divider);
+
 		return number;
-		
+
 	}
 
-	public Boolean isZeroed()
-	{
-		if ( this.getNumerator() == 0 && this.getDenominator() == 0 )
-		{
+	public Boolean isZeroed() {
+		if (this.getNumerator() == 0 && this.getDenominator() == 0) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 
