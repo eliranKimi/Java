a2fc75814c62e730538752827e6b2bb885e47f01
eliran <eliran.kimi@gmail.com>
2017-11-14 09:27:19 +0200

    Added default constructors on substraction and addition (testing push)

--- maman12/src/maman12/AdditionExpression.java
+++ maman12/src/maman12/AdditionExpression.java
@@ -2,4 +2,18 @@ package maman12;
 
 public class AdditionExpression extends CompoundExpression{
 
+	public AdditionExpression(Expression firstOperand, Expression secondOperand) {
+		super(firstOperand, secondOperand);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double calcualte() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+
 }
--- maman12/src/maman12/SubtractionExpression.java
+++ maman12/src/maman12/SubtractionExpression.java
@@ -2,4 +2,15 @@ package maman12;
 
 public class SubtractionExpression extends CompoundExpression{
 
+	public SubtractionExpression(Expression firstOperand, Expression secondOperand) {
+		super(firstOperand, secondOperand);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double calcualte() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
