9973bbe3f505f1c71da0ea37d57d9b6d174f2288
eliran <eliran.kimi@gmail.com>
2017-12-31 15:09:42 +0200

    Added all functions from question 1A

--- maman14-Q1/src/GroupSet.java
+++ maman14-Q1/src/GroupSet.java
@@ -1,4 +1,80 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
-public class GroupSet {
+public class GroupSet<T> {
+
+	private ArrayList<T> genericSet;
+
+	public GroupSet() {
+		this.genericSet = new ArrayList<T>();
+	}
+
+	public void union(GroupSet<T> aSet) {
+
+		Iterator<T> it = aSet.iterator();
+
+		while (it.hasNext()) {
+			this.insert(it.next());
+		}
+
+	}
+
+	public void intersect(GroupSet<T> aSet) {
+
+		Iterator<T> it = aSet.iterator();
+		ArrayList<T> intersectSet = new ArrayList<T>();
+
+		while (it.hasNext()) {
+
+			T aMember = it.next();
+
+			if (genericSet.contains(aMember) == true) {
+				intersectSet.add(aMember);
+			}
+
+		}
+
+		this.genericSet = intersectSet;
+
+	}
+
+	public boolean isSubset(GroupSet<T> aSet) {
+
+		Iterator<T> it = aSet.iterator();
+
+		while (it.hasNext()) {
+
+			if (this.genericSet.contains(it.next()) == false) {
+				return false;
+			}
+
+		}
+		return true;
+	}
+
+	public boolean isMember(T aMember) {
+		return this.genericSet.contains(aMember);
+	}
+
+	public void insert(T aMember) {
+		if (this.genericSet.contains(aMember) == false) {
+			this.genericSet.add(aMember);
+		}
+	}
+
+	public void delete(T aMember) {
+
+		if (this.genericSet.contains(aMember) == false) {
+			this.genericSet.remove(aMember);
+		}
+
+	}
+
+	public Iterator<T> iterator() {
+
+		return this.genericSet.iterator();
+
+	}
 
 }
